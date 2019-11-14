7dae408d72d44a0b14dd06eafb164f098807ac1c
skuethe <crazycapivara@gmail.com>
2019-11-14 19:32:27 +0100

    Initial commit

---
+++ R/widget.R
@@ -0,0 +1,6 @@
+#' @export
+make_widget <- function(name, data, element_id) {
+  widget_lib <- system.file("dist/rwidget.js", package = utils::packageName())
+  message(widget_lib)
+  message("not implemented yet")
+}
---
+++ javascript/src/index.js
@@ -0,0 +1,1 @@
+console.log("Pipe data from R to JavaScript.");
