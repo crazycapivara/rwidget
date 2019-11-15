22a28e88d23a1e5c16b066eaaf7c928a4c1a8919
skuethe <crazycapivara@gmail.com>
2019-11-15 19:48:33 +0100

    Support external scripts

--- R/widget.R
+++ R/widget.R
@@ -1,11 +1,12 @@
 #' @export
-make_widget <- function(name, data, element_id, script) {
+make_widget <- function(name, script, data, element_id, external_scripts = NULL) {
   structure(
     list(
       name = name,
+      script = script,
       element_id = element_id,
       dataset = data,
-      script = script
+      external_scripts = external_scripts
     ),
     class = "rwidget"
   )
@@ -14,11 +15,23 @@ make_widget <- function(name, data, element_id, script) {
 #' @export
 write_html <- function(widget, output_folder = tempdir()) {
   html <- readr::read_file(get_file(CONFIG$template)) %>%
+    stringr::str_replace("<!-- head -->", make_head(widget)) %>%
     stringr::str_replace("\\{\\{ rwidget \\}\\}", make_widget_tag(widget))
   index_filename <- file.path(output_folder, "index.html")
   readr::write_file(html, index_filename)
   file.copy(c(get_file(CONFIG$lib), widget$script), output_folder, overwrite = TRUE)
   index_filename
+}
+
+make_head <- function(widget) {
+  if (is.null(widget$external_scripts)) {
+    return("")
+  }
+
+  lapply(widget$external_scripts, function(script) {
+    htmltools::tags$script(src = script)
+  }) %>% htmltools::tagList() %>%
+    as.character()
 }
 
 make_widget_tag <- function(widget) {
