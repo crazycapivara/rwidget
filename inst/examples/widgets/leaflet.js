3e0fca14d95f50b041a036a439e8a60de701d304
skuethe <crazycapivara@gmail.com>
2019-11-18 18:08:20 +0100

    Update examples

--- inst/examples/widgets/leaflet.js
+++ inst/examples/widgets/leaflet.js
@@ -1,11 +1,8 @@
 function leafletWidget(widgetElement) {
   const widget = { };
 
-  // widgetElement.style.height = "600px";
-
   widget.render = function(widgetData) {
     console.log(L);
-    // widgetElement.innerHTML = widgetData.message;
     const map = window.map = L.map(widgetElement.id).setView([51.505, -0.09], 13);
     L.tileLayer("https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
       attribution: "Add attribution here ...",
